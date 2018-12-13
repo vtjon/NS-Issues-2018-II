49b2201b967f11a75f418291c0f12baba7bc7e5d
Jonathan Austin <jonathan.austin@gmail.com>
2018-12-13 08:59:22 -0500

    add delegate

---
+++ nativescript-dev-webpack/issue-625/app/app.component.android.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+import * as app from "application";
+import * as uiHelper from "~/common/ui-helper";
+
+@Component({
+  selector: "ns-app",
+  templateUrl: "app.component.html"
+})
+export class AppComponent implements OnInit {
+  ngOnInit() {}
+}
